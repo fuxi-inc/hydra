b7dde23121626187f205ce92cbef04e975ee805f
zhangyouyi125 <1197902754@qq.com>
2021-12-22 11:39:13 +0800

    refine code

---
+++ .history/client/handler_20211222113758.go
@@ -0,0 +1,423 @@
+/*
+ * Copyright © 2015-2018 Aeneas Rekkas <aeneas+oss@aeneas.io>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author		Aeneas Rekkas <aeneas+oss@aeneas.io>
+ * @copyright 	2015-2018 Aeneas Rekkas <aeneas+oss@aeneas.io>
+ * @license 	Apache-2.0
+ */
+
+package client
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/ory/fosite"
+
+	"github.com/ory/x/errorsx"
+
+	"github.com/ory/herodot"
+	"github.com/ory/x/sqlcon"
+
+	"github.com/ory/hydra/x"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/pkg/errors"
+
+	"github.com/ory/x/pagination"
+)
+
+type Handler struct {
+	r InternalRegistry
+}
+
+const (
+	ClientsHandlerPath    = "/clients"
+	LicensesHandlerPath   = "/licenses"
+	NamespacesHandlerPath = "/namespaces"
+)
+
+func NewHandler(r InternalRegistry) *Handler {
+	return &Handler{
+		r: r,
+	}
+}
+
+func (h *Handler) SetRoutes(admin *x.RouterAdmin) {
+	admin.GET(ClientsHandlerPath, h.List)
+	admin.POST(ClientsHandlerPath, h.Create)
+	admin.GET(ClientsHandlerPath+"/:id", h.Get)
+	admin.PUT(ClientsHandlerPath+"/:id", h.Update)
+	admin.PATCH(ClientsHandlerPath+"/:id", h.Patch)
+	admin.DELETE(ClientsHandlerPath+"/:id", h.Delete)
+	admin.GET(ClientsHandlerPath+"/:id"+LicensesHandlerPath, h.GetLicenses)
+	admin.POST(ClientsHandlerPath+"/:id"+LicensesHandlerPath, h.CreateLicense)
+	admin.GET(ClientsHandlerPath+"/:id"+NamespacesHandlerPath, h.GetNamespaces)
+}
+
+// swagger:route POST /clients admin createOAuth2Client
+//
+// Create an OAuth 2.0 Client
+//
+// Create a new OAuth 2.0 client If you pass `client_secret` the secret will be used, otherwise a random secret will be generated. The secret will be returned in the response and you will not be able to retrieve it later on. Write the secret down and keep it somwhere safe.
+//
+// OAuth 2.0 clients are used to perform OAuth 2.0 and OpenID Connect flows. Usually, OAuth 2.0 clients are generated for applications which want to consume your OAuth 2.0 or OpenID Connect capabilities. To manage ORY Hydra, you will need an OAuth 2.0 Client as well. Make sure that this endpoint is well protected and only callable by first-party components.
+//
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Schemes: http, https
+//
+//     Responses:
+//       201: oAuth2Client
+//       400: jsonError
+//       409: jsonError
+//       500: jsonError
+func (h *Handler) Create(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var c Client
+
+	if err := json.NewDecoder(r.Body).Decode(&c); err != nil {
+		h.r.Writer().WriteError(w, r, errorsx.WithStack(err))
+		return
+	}
+
+	if len(c.Secret) == 0 {
+		secretb, err := x.GenerateSecret(26)
+		if err != nil {
+			h.r.Writer().WriteError(w, r, errorsx.WithStack(err))
+			return
+		}
+		c.Secret = string(secretb)
+	}
+
+	if err := h.r.ClientValidator().Validate(&c); err != nil {
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
+
+	secret := c.Secret
+	c.CreatedAt = time.Now().UTC().Round(time.Second)
+	c.UpdatedAt = c.CreatedAt
+
+	if err := h.r.ClientManager().CreateClient(r.Context(), &c); err != nil {
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
+
+	c.Secret = ""
+	if !c.IsPublic() {
+		c.Secret = secret
+	}
+	h.r.Writer().WriteCreated(w, r, ClientsHandlerPath+"/"+c.GetID(), &c)
+}
+
+// swagger:route PUT /clients/{id} admin updateOAuth2Client
+//
+// Update an OAuth 2.0 Client
+//
+// Update an existing OAuth 2.0 Client. If you pass `client_secret` the secret will be updated and returned via the API. This is the only time you will be able to retrieve the client secret, so write it down and keep it safe.
+//
+// OAuth 2.0 clients are used to perform OAuth 2.0 and OpenID Connect flows. Usually, OAuth 2.0 clients are generated for applications which want to consume your OAuth 2.0 or OpenID Connect capabilities. To manage ORY Hydra, you will need an OAuth 2.0 Client as well. Make sure that this endpoint is well protected and only callable by first-party components.
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Schemes: http, https
+//
+//     Responses:
+//       200: oAuth2Client
+//       500: jsonError
+func (h *Handler) Update(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var c Client
+
+	if err := json.NewDecoder(r.Body).Decode(&c); err != nil {
+		h.r.Writer().WriteError(w, r, errorsx.WithStack(err))
+		return
+	}
+
+	c.OutfacingID = ps.ByName("id")
+	if err := h.updateClient(r.Context(), &c); err != nil {
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
+
+	h.r.Writer().Write(w, r, &c)
+}
+
+// swagger:route PATCH /clients/{id} admin patchOAuth2Client
+//
+// Patch an OAuth 2.0 Client
+//
+// Patch an existing OAuth 2.0 Client. If you pass `client_secret` the secret will be updated and returned via the API. This is the only time you will be able to retrieve the client secret, so write it down and keep it safe.
+//
+// OAuth 2.0 clients are used to perform OAuth 2.0 and OpenID Connect flows. Usually, OAuth 2.0 clients are generated for applications which want to consume your OAuth 2.0 or OpenID Connect capabilities. To manage ORY Hydra, you will need an OAuth 2.0 Client as well. Make sure that this endpoint is well protected and only callable by first-party components.
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Schemes: http, https
+//
+//     Responses:
+//       200: oAuth2Client
+//       500: jsonError
+func (h *Handler) Patch(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	patchJSON, err := io.ReadAll(r.Body)
+	if err != nil {
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
+
+	id := ps.ByName("id")
+	c, err := h.r.ClientManager().GetConcreteClient(r.Context(), id)
+	if err != nil {
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
+
+	if err := x.ApplyJSONPatch(patchJSON, c, "/id"); err != nil {
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
+
+	if err := h.updateClient(r.Context(), c); err != nil {
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
+
+	h.r.Writer().Write(w, r, c)
+}
+
+func (h *Handler) updateClient(ctx context.Context, c *Client) error {
+	var secret string
+	if len(c.Secret) > 0 {
+		secret = c.Secret
+	}
+	if err := h.r.ClientValidator().Validate(c); err != nil {
+		return err
+	}
+
+	c.UpdatedAt = time.Now().UTC().Round(time.Second)
+	if err := h.r.ClientManager().UpdateClient(ctx, c); err != nil {
+		return err
+	}
+	c.Secret = secret
+	return nil
+}
+
+// swagger:parameters listOAuth2Clients
+type Filter struct {
+	// The maximum amount of clients to returned, upper bound is 500 clients.
+	// in: query
+	Limit int `json:"limit"`
+
+	// The offset from where to start looking.
+	// in: query
+	Offset int `json:"offset"`
+
+	// The name of the clients to filter by.
+	// in: query
+	Name string `json:"client_name"`
+
+	// The owner of the clients to filter by.
+	// in: query
+	Owner string `json:"owner"`
+}
+
+// swagger:route GET /clients admin listOAuth2Clients
+//
+// List OAuth 2.0 Clients
+//
+// This endpoint lists all clients in the database, and never returns client secrets. As a default it lists the first 100 clients. The `limit` parameter can be used to retrieve more clients, but it has an upper bound at 500 objects. Pagination should be used to retrieve more than 500 objects.
+//
+// OAuth 2.0 clients are used to perform OAuth 2.0 and OpenID Connect flows. Usually, OAuth 2.0 clients are generated for applications which want to consume your OAuth 2.0 or OpenID Connect capabilities. To manage ORY Hydra, you will need an OAuth 2.0 Client as well. Make sure that this endpoint is well protected and only callable by first-party components.
+// The "Link" header is also included in successful responses, which contains one or more links for pagination, formatted like so: '<https://hydra-url/admin/clients?limit={limit}&offset={offset}>; rel="{page}"', where page is one of the following applicable pages: 'first', 'next', 'last', and 'previous'.
+// Multiple links can be included in this header, and will be separated by a comma.
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Schemes: http, https
+//
+//     Responses:
+//       200: oAuth2ClientList
+//       500: jsonError
+func (h *Handler) List(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	limit, offset := pagination.Parse(r, 100, 0, 500)
+	filters := Filter{
+		Limit:  limit,
+		Offset: offset,
+		Name:   r.URL.Query().Get("client_name"),
+		Owner:  r.URL.Query().Get("owner"),
+	}
+
+	c, err := h.r.ClientManager().GetClients(r.Context(), filters)
+	if err != nil {
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
+
+	for k := range c {
+		c[k].Secret = ""
+	}
+
+	n, err := h.r.ClientManager().CountClients(r.Context())
+	if err != nil {
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
+
+	pagination.Header(w, r.URL, n, limit, offset)
+
+	if c == nil {
+		c = []Client{}
+	}
+
+	h.r.Writer().Write(w, r, c)
+}
+
+// swagger:route GET /clients/{id} admin getOAuth2Client
+//
+// Get an OAuth 2.0 Client.
+//
+// Get an OAUth 2.0 client by its ID. This endpoint never returns passwords.
+//
+// OAuth 2.0 clients are used to perform OAuth 2.0 and OpenID Connect flows. Usually, OAuth 2.0 clients are generated for applications which want to consume your OAuth 2.0 or OpenID Connect capabilities. To manage ORY Hydra, you will need an OAuth 2.0 Client as well. Make sure that this endpoint is well protected and only callable by first-party components.
+//
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Schemes: http, https
+//
+//     Responses:
+//       200: oAuth2Client
+//       401: jsonError
+//       500: jsonError
+func (h *Handler) Get(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var id = ps.ByName("id")
+
+	c, err := h.r.ClientManager().GetConcreteClient(r.Context(), id)
+	if err != nil {
+		if errors.Is(err, sqlcon.ErrNoRows) {
+			err = herodot.ErrUnauthorized.WithReason("The requested OAuth 2.0 client does not exist or you did not provide the necessary credentials")
+		}
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
+
+	c.Secret = ""
+	h.r.Writer().Write(w, r, c)
+}
+
+// swagger:route DELETE /clients/{id} admin deleteOAuth2Client
+//
+// Deletes an OAuth 2.0 Client
+//
+// Delete an existing OAuth 2.0 Client by its ID.
+//
+// OAuth 2.0 clients are used to perform OAuth 2.0 and OpenID Connect flows. Usually, OAuth 2.0 clients are generated for applications which want to consume your OAuth 2.0 or OpenID Connect capabilities. To manage ORY Hydra, you will need an OAuth 2.0 Client as well. Make sure that this endpoint is well protected and only callable by first-party components.
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Schemes: http, https
+//
+//     Responses:
+//       204: emptyResponse
+//       404: jsonError
+//       500: jsonError
+func (h *Handler) Delete(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var id = ps.ByName("id")
+
+	if err := h.r.ClientManager().DeleteClient(r.Context(), id); err != nil {
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (h *Handler) CreateLicense(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	clientID := ps.ByName("id")
+
+	clientSecret := fosite.AccessTokenFromRequest(r)
+
+	if clientID == "" || clientSecret == "" {
+		h.r.Writer().WriteError(w, r, errors.New("invalid argument"))
+		return
+	}
+	entity, err := h.r.ClientManager().CreateLicense(r.Context(), clientID, clientSecret)
+	if err != nil {
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
+
+	h.r.Writer().WriteCode(w, r, 200, entity)
+}
+
+func (h *Handler) GetLicenses(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var clientID = ps.ByName("id")
+	clientSecret := fosite.AccessTokenFromRequest(r)
+
+	if clientID == "" || clientSecret == "" {
+		h.r.Writer().WriteError(w, r, errors.New("invalid argument"))
+		return
+	}
+
+	entities, err := h.r.ClientManager().GetLicenses(r.Context(), clientID, clientSecret)
+	if err != nil {
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
+
+	h.r.Writer().Write(w, r, entities)
+}
+
+func (h *Handler) GetNamespaces(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var clientID = ps.ByName("id")
+	apiKey := fosite.AccessTokenFromRequest(r)
+
+	if clientID == "" || apiKey == "" {
+		h.r.Writer().WriteError(w, r, errors.New("invalid argument"))
+		return
+	}
+
+	ctx := context.WithValue(context.TODO(), "apiKey", apiKey)
+	result := h.r.ClientManager().AvailableNamespaces(ctx)
+
+	h.r.Writer().Write(w, r, result)
+}
---
+++ .history/identifier/handler_20211222113744.go
@@ -0,0 +1,175 @@
+package identifier
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/ory/fosite"
+	"github.com/ory/x/errorsx"
+	"github.com/ory/x/pagination"
+	"go.uber.org/zap"
+
+	"github.com/ory/hydra/internal/logger"
+	"github.com/ory/hydra/x"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type Handler struct {
+	r InternalRegistry
+}
+
+const (
+	IdentifierHandlerPath = "/identifier"
+)
+
+func NewHandler(r InternalRegistry) *Handler {
+	return &Handler{
+		r: r,
+	}
+}
+
+func (h *Handler) SetRoutes(public *x.RouterPublic) {
+	public.POST(IdentifierHandlerPath, h.Create)
+	public.GET(IdentifierHandlerPath+"/:id", h.Get)
+	public.DELETE(IdentifierHandlerPath+"/:id", h.Delete)
+	public.GET(IdentifierHandlerPath, h.List)
+}
+
+func (h *Handler) Create(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var entity Identifier
+
+	if err := json.NewDecoder(r.Body).Decode(&entity); err != nil {
+		h.r.Writer().WriteError(w, r, errorsx.WithStack(err))
+		return
+	}
+
+	if err := h.r.IdentifierValidator().Validate(&entity); err != nil {
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
+
+	accessToken := fosite.AccessTokenFromRequest(r)
+
+	if accessToken == "" {
+		h.r.Writer().WriteError(w, r, errors.New(""))
+		return
+	}
+
+	entity.AuthAddress = "http://localhost:4444"
+	entity.DataSignature = nil
+
+	ctx := context.WithValue(context.TODO(), "apiKey", accessToken)
+	err := h.r.IdentifierManager().CreateIdentifier(ctx, &entity)
+	if err != nil {
+		logger.Get().Warnw("failed to create identity identifier", zap.Error(err), zap.Any("entity", entity))
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
+
+	h.r.Writer().WriteCreated(w, r, IdentifierHandlerPath+"/"+entity.ID, &entity)
+}
+
+func (h *Handler) Update(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+}
+
+// swagger:parameters listOAuth2Clients
+type Filter struct {
+	Limit    int    `json:"limit"`
+	Offset   int    `json:"offset"`
+	ClientId string `json:"client_id"`
+	Tag      string `json:"tag"`
+	Metadata string `json:"metadata"`
+}
+
+func (h *Handler) List(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	limit, offset := pagination.Parse(r, 100, 0, 500)
+	filters := Filter{
+		Limit:    limit,
+		Offset:   offset,
+		ClientId: r.URL.Query().Get("client_id"),
+		Tag:      r.URL.Query().Get("tag"),
+		Metadata: r.URL.Query().Get("metadata"),
+	}
+
+	accessToken := fosite.AccessTokenFromRequest(r)
+
+	if accessToken == "" {
+		h.r.Writer().WriteError(w, r, errors.New(""))
+		return
+	}
+
+	ctx := context.WithValue(context.TODO(), "apiKey", accessToken)
+	c, err := h.r.IdentifierManager().GetIdentifiers(ctx, filters)
+	if err != nil {
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
+
+	// TODO should get real total count
+	pagination.Header(w, r.URL, 10000, limit, offset)
+
+	if c == nil {
+		c = []*Identifier{}
+	}
+
+	h.r.Writer().Write(w, r, c)
+}
+
+func (h *Handler) Get(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var id = ps.ByName("id")
+
+	accessToken := fosite.AccessTokenFromRequest(r)
+
+	if accessToken == "" {
+		h.r.Writer().WriteError(w, r, errors.New(""))
+		return
+	}
+
+	ctx := context.WithValue(context.TODO(), "apiKey", accessToken)
+	entity, err := h.r.IdentifierManager().GetIdentifier(ctx, id)
+	if err != nil {
+		//err = herodot.ErrUnauthorized.WithReason("")
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
+	if entity == nil {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+
+	h.r.Writer().Write(w, r, entity)
+}
+
+func (h *Handler) Delete(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var id = ps.ByName("id")
+
+	accessToken := fosite.AccessTokenFromRequest(r)
+
+	if accessToken == "" {
+		h.r.Writer().WriteError(w, r, errors.New(""))
+		return
+	}
+
+	ctx := context.WithValue(context.TODO(), "apiKey", accessToken)
+	entity, err := h.r.IdentifierManager().GetIdentifier(ctx, id)
+	if err != nil {
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
+	if entity == nil {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+
+	err = h.r.IdentifierManager().DeleteIdentifier(ctx, id)
+	if err != nil {
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
