e933e8de5263f0064432c42c07960bb7bf31e3a5
zhangyouyi125 <1197902754@qq.com>
2021-12-23 14:20:21 +0800

    test

--- .history/subscription/handler_20211223141953.go
+++ .history/subscription/handler_20211223141953.go
@@ -119,7 +119,7 @@ func (h *Handler) Create(w http.ResponseWriter, r *http.Request, _ httprouter.Pa
 	logger.Get().Infow("subscription", zap.Any("data", entity))
 
 	ctx := context.WithValue(context.TODO(), "apiKey", accessToken)
-	err = h.r.SubscriptionManager().CreateSubscription(ctx, &entity)
+	err := h.r.SubscriptionManager().CreateSubscription(ctx, &entity)
 	if err != nil {
 		h.r.Writer().WriteError(w, r, err)
 		return
