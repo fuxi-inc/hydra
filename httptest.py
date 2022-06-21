15caed373f284c556083b6b5472272389295e71a
Lyinger <517799460@qq.com>
2022-06-21 23:33:21 +0800

    refine httptest

--- httptest.py
+++ httptest.py
@@ -5,37 +5,36 @@ from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5 as PKCS1_signature
 from Crypto.Hash import SHA1
 
-
-def http_request(route, params):
-    url = "http:localhost:4444/"+route
-    r = requests.post(url, params)
-    return r
-
 def get_key(key_file):
     with open(key_file) as f:
         data = f.read()
         key = RSA.importKey(data)
     return key
 
-def test_authorization(argv):
-    if argv[0] == "addAuth":
-        params = {}
-        for arg in argv[1:]:
-            params[arg.split("=")[0]] = arg.split("=")[1]
-        params["sign"] = ""
+def get_params(argv):
+    params = {}
+    for arg in argv:
+        params[arg.split("=")[0]] = arg.split("=")[1]
+    params["sign"] = ""
 
-        data = json.dumps(params)
-        hashdata = SHA1.new()
-        hashdata.update(data.encode("utf8"))
+    data = json.dumps(params)
+    hashdata = SHA1.new()
+    hashdata.update(data.encode("utf8"))
 
-        private_key = get_key('files/private.pem')
-        signer = PKCS1_signature.new(private_key)
-        params["sign"] = signer.sign(hashdata)
-
-        print(http_request("authorization/addAuth", params))
+    private_key = get_key('files/private.pem')
+    signer = PKCS1_signature.new(private_key)
+    params["sign"] = signer.sign(hashdata)
+    print(params)
 
 
 if __name__ == '__main__':
-    method = sys.argv[1]
-    if method == "authorization":
-        test_authorization(sys.argv[2:])
+    url = sys.argv[2]
+    resp = requests.Response
+    if sys.argv[1] == "get":
+        resp = requests.get(url)
+        print(resp)
+        print(resp.text)
+    if sys.argv[2] == "post":
+        resp = requests.post(url, get_params(sys.argv[3:]))
+        print(resp)
+        print(resp.text)
