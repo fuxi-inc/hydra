4060248b8ceb5c9977ade35a3185759e305f171a
VincentFvan <994992433@qq.com>
2022-06-22 15:15:49 +0800

    n

--- persistence/sql/persister_identifier.go
+++ persistence/sql/persister_identifier.go
@@ -5,8 +5,6 @@ import (
 	"crypto"
 	"crypto/rsa"
 	"crypto/x509"
-	"encoding/pem"
-	"os"
 	"strings"
 
 	"github.com/ory/hydra/identifier"
@@ -120,16 +118,6 @@ func (p *Persister) VerifySignature(ctx context.Context, userID string, sign str
 	}
 
 	publicKey, _ := x509.ParsePKCS1PublicKey(cl.PublicKey)
-
-	// logger.Get().Infow(cl.ID, zap.Error(err))
-	// logger.Get().Infow(string(cl.PublicKey), zap.Error(err))
-	file, err := os.Create("./files/public2.pem")
-
-	block := &pem.Block{
-		Type:  "PUBLIC KEY",
-		Bytes: cl.PublicKey,
-	}
-	err = pem.Encode(file, block)
 
 	err = rsa.VerifyPKCS1v15(publicKey, crypto.SHA1, hash, []byte(sign))
 	if err != nil {
