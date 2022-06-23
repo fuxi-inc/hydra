09b9f718575f6c46e09b2d8b3813f7827a6b7c5f
VincentFvan <994992433@qq.com>
2022-06-23 14:46:07 +0800

    test sign

--- identifier/validator.go
+++ identifier/validator.go
@@ -12,7 +12,7 @@ func NewValidator() *Validator {
 }
 
 func (v *Validator) Validate(jsonTrans *JSONTrans) error {
-	if jsonTrans.DataID == "" || jsonTrans.UserID == "" || jsonTrans.DataAddress == "" || jsonTrans.DataDigest == "" || jsonTrans.Sign == "" {
+	if jsonTrans.DataID == "" || jsonTrans.UserID == "" || jsonTrans.DataAddress == "" || jsonTrans.DataDigest == nil || jsonTrans.Sign == nil {
 		return errorsx.WithStack(ErrInvalidIdentifierMetadata.WithHint("DataID, UserID, DataAddress, DataDigest, Sign must be set."))
 	}
 	return nil
