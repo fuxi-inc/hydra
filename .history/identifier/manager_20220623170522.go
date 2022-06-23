67fe5641211d69c2930b91e9aa1fea7524f9a71f
VincentFvan <994992433@qq.com>
2022-06-23 22:39:41 +0800

    refine code

---
+++ .history/identifier/manager_20220623170522.go
@@ -0,0 +1,21 @@
+package identifier
+
+import (
+	"context"
+)
+
+type Manager interface {
+	Storage
+}
+
+type Storage interface {
+	GetIdentifier(ctx context.Context, id string) (*Identifier, error)
+
+	CreateIdentifier(ctx context.Context, entity *Identifier) error
+
+	DeleteIdentifier(ctx context.Context, id string) error
+
+	GetIdentifiers(ctx context.Context, filters Filter) ([]*Identifier, error)
+
+	VerifySignature(ctx context.Context, userID string, sign []byte, hash []byte) error
+}
