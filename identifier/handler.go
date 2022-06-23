76198eac47846bc2651fab5ca0fd8c49a3f7c6ed
VincentFvan <994992433@qq.com>
2022-06-23 14:55:07 +0800

    test sign

--- identifier/handler.go
+++ identifier/handler.go
@@ -113,8 +113,8 @@ func (h *Handler) Create(w http.ResponseWriter, r *http.Request, _ httprouter.Pa
 	logger.Get().Infow("output marshalJsonTrans", zap.Any("action", string(marshalJsonTrans)))
 
 	hash := crypto.SHA1.New()
-	// hash.Write([]byte("DIS_2020" + string(marshalJsonTrans)))
-	hash.Write(marshalJsonTrans)
+	hash.Write([]byte("DIS_2020" + string(marshalJsonTrans)))
+	// hash.Write(marshalJsonTrans)
 	verifyHash := hash.Sum(nil)
 
 	ctx := context.Background()
