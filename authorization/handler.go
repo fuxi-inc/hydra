e443178c22481b356f82d332f63acaec626eef55
Lyinger <517799460@qq.com>
2022-06-23 16:39:18 +0800

    update authorization authentication params

--- authorization/handler.go
+++ authorization/handler.go
@@ -261,7 +261,7 @@ func (h *Handler) CreateAuthzTrans(w http.ResponseWriter, r *http.Request, _ htt
 //       404: jsonError
 //       500: jsonError
 func (h *Handler) Authenticate(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	var params AuthorizationParams
+	var params AuthenticationParams
 	json.NewDecoder(r.Body)
 	if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
 		logger.Get().Infow("failed to decode params")
@@ -269,7 +269,7 @@ func (h *Handler) Authenticate(w http.ResponseWriter, r *http.Request, _ httprou
 		return
 	}
 
-	if err := h.r.AuthorizationValidator().Validate(&params); err != nil {
+	if err := h.r.AuthorizationValidator().ValidateAuthenticationParam(&params); err != nil {
 		logger.Get().Infow("failed to validate authentication params", zap.Error(err))
 		h.r.Writer().WriteError(w, r, err)
 		return
--- authorization/validator.go
+++ authorization/validator.go
@@ -23,3 +23,19 @@ func (v *Validator) Validate(params *AuthorizationParams) error {
 	}
 	return nil
 }
+
+func (v *Validator) ValidateAuthenticationParam(params *AuthenticationParams) error {
+	if params.Sign == "" {
+		return errorsx.WithStack(ErrInvalidAuthorizationParams.WithHint("Request must be signed"))
+	}
+	if params.SignRecipient == "" {
+		return errorsx.WithStack(ErrInvalidAuthorizationParams.WithHint("ViewUser must signed the request"))
+	}
+	if params.Identifier == "" {
+		return errorsx.WithStack(ErrInvalidAuthorizationParams.WithHint("Data identifier must be provided"))
+	}
+	if params.Recipient == "" {
+		return errorsx.WithStack(ErrInvalidAuthorizationParams.WithHint("ViewUser identifier must be provided"))
+	}
+	return nil
+}
