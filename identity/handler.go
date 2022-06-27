77ccfe98bcfd541b76056fac26de56a7ab0c0d7f
VincentFvan <994992433@qq.com>
2022-06-27 17:40:54 +0800

    create user for test

--- identity/handler.go
+++ identity/handler.go
@@ -8,8 +8,10 @@ import (
 	"crypto/x509"
 	"encoding/hex"
 	"encoding/json"
+	"encoding/pem"
 	"errors"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
 
@@ -130,39 +132,41 @@ func (h *Handler) Create(w http.ResponseWriter, r *http.Request, _ httprouter.Pa
 
 	entity.PublicKey = x509.MarshalPKCS1PublicKey(publickey)
 
-	// // 创建私钥pem文件
-	// file, err := os.Create("./files/private.pem")
-	// if err != nil {
-	// 	h.r.Writer().WriteError(w, r, err)
-	// 	return
-	// }
-	// // 对私钥信息进行编码，写入到私钥文件中
-	// block := &pem.Block{
-	// 	Type:  "RSA PRIVATE KEY",
-	// 	Bytes: entity.PrivateKey,
-	// }
-	// err = pem.Encode(file, block)
-	// if err != nil {
-	// 	h.r.Writer().WriteError(w, r, err)
-	// 	return
-	// }
+	// 创建私钥pem文件
+	filestring := "./files/private" + entity.ID + ".pem"
+	file, err := os.Create(filestring)
+	if err != nil {
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
+	// 对私钥信息进行编码，写入到私钥文件中
+	block := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: entity.PrivateKey,
+	}
+	err = pem.Encode(file, block)
+	if err != nil {
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
 
-	// // 创建公钥pem文件
-	// file, err = os.Create("./files/public.pem")
-	// if err != nil {
-	// 	h.r.Writer().WriteError(w, r, err)
-	// 	return
-	// }
-	// // 对公钥信息进行编码，写入公钥文件中
-	// block = &pem.Block{
-	// 	Type:  "PUBLIC KEY",
-	// 	Bytes: entity.PublicKey,
-	// }
-	// err = pem.Encode(file, block)
-	// if err != nil {
-	// 	h.r.Writer().WriteError(w, r, err)
-	// 	return
-	// }
+	// 创建公钥pem文件
+	filestring = "./files/public" + entity.ID + ".pem"
+	file, err = os.Create(filestring)
+	if err != nil {
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
+	// 对公钥信息进行编码，写入公钥文件中
+	block = &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: entity.PublicKey,
+	}
+	err = pem.Encode(file, block)
+	if err != nil {
+		h.r.Writer().WriteError(w, r, err)
+		return
+	}
 
 	// rng := rand.Reader
 
